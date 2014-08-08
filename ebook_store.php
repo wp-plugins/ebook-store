fc07f904deff285a820cfbb51a60ff244ba1366d
motov.net <motov.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-08 08:21:22 +0000

    Fix for WPMU where IPN was failing.
    git-svn-id: https://plugins.svn.wordpress.org/ebook-store/trunk@962260 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ebook_store.php
+++ ebook_store.php
@@ -5,75 +5,80 @@ Plugin URI: http://shopfiles.com/
 Description: A powerful tool for selling ebooks with wordpress
 Author: Deian Motov
 Author URI:http://motov.net/
-Version: 2.3
+Version: 2.4
 License: GPLv2
 */
+
 include_once('functions.php');
 include_once('class_qswpoptions.php');
 include_once('ebook_options.php');
 
-if ($_REQUEST['task'] == 'ipn') {
-	global $ebook_email_delivery;
-	include_once 'payment_gateways/paypal/ipnlistener.php';
-	$listener = new IpnListener();
-	if (get_option('paypal_sandbox') > 0) {
-		$listener->use_sandbox = true;
-	}
-	try {
-		$listener->requirePostMethod();
-		$verified = $listener->processIpn();
-	} catch (Exception $e) {
-		error_log($e->getMessage());
-		exit(0);
-	}
-	if ($verified) {
-		$my_post = array(
-		  'post_title'    => $_REQUEST['first_name'] . ' ' . $_REQUEST['last_name'],
-		  'post_type'	  => 'ebook_order',
-		  'post_status'   => 'publish',
-		  'post_author'   => 1,
-		  'post_category' => array(8,39));
-		$custom = explode("|",$_REQUEST['custom']);
-		$mc_gross = number_format($_REQUEST['mc_gross'], 2, '.', ',');
-		$vat = get_option('vat_percent'); 
-		if ($vat > 0) {
-			$mc_gross = $_REQUEST['mc_gross'] - $_REQUEST['tax'];
-		}
-		$mc_gross = number_format($mc_gross, 2, '.', ',');
-		if (md5(NONCE_KEY . $custom[0] . $mc_gross) == $custom[1]) {
-			$post_id = wp_insert_post( $my_post, $wp_error );
-			foreach ($_REQUEST as $k => $v) {
-				update_post_meta($post_id, $k, $v);
-				@$order[$k] = $v;
-			}
-			$ebook_key = preg_replace("/[^a-zA-Z0-9]+/", "", $_REQUEST['ebook_key']);
-			$order['order_id'] = $post_id;
-			$order['password'] = $_REQUEST['payer_email'];
-			$ebook_order['ebook_key'][0] = $ebook_key;
-			$ebook_order['ebook'][0] = $custom[0];
-			$order['downloadlink'] = ebook_download_link($ebook_order);
-			update_post_meta($post_id,'ebook_key',$ebook_key);
-			update_post_meta($post_id,'downloads',0);
-			update_post_meta($post_id,'ebook',$custom[0]);
-			$attachment = ebook_attachment($custom[0]);
-			global $attachment;
-			mail(get_option( 'admin_email' ), 'eBook store for WordPress - Verified Order Received', $listener->getTextReport() . print_r($attachment,true));
-			//wp_mail($_REQUEST['payer_email'],get_option('email_delivery_subject'),'Email delivery text');
-			$ebook_email_delivery = array('to' => $_REQUEST['payer_email'], 'subject' => get_option('email_delivery_subject'), 'text' => get_option('email_delivery_text'),'attachment' => $attachment, 'order' => $order);
-			if (pathinfo($attachment[0]['file'],PATHINFO_EXTENSION) == 'pdf' && get_option('encrypt_pdf')) {
-				add_action( 'plugins_loaded', 'ebook_encrypt_pdf', 99 );
-			}
-			
-			add_action( 'plugins_loaded', 'ebook_email_delivery', 100 );
-			//array('to' => $_REQUEST['payer_email'], 'subject' => get_option('email_delivery_subject'), 'text' => 'teeext', 'file' => $attachment[0]['file'])
-		} else {
-			mail(get_option( 'admin_email' ), 'eBook store for WordPress - Possible fraud attempt ', $listener->getTextReport());
-		}
+add_action('init', 'check_ipn');
 
-	} else {
-		mail(get_option( 'admin_email' ), 'eBook store for WordPress - Possible fraud attempt', $listener->getTextReport());
-	}
+function check_ipn() {
+	if ($_REQUEST['task'] == 'ipn') {
+		global $ebook_email_delivery;
+		include_once 'payment_gateways/paypal/ipnlistener.php';
+		$listener = new IpnListener();
+		if (get_option('paypal_sandbox') > 0) {
+			$listener->use_sandbox = true;
+		}
+		try {
+			$listener->requirePostMethod();
+			$verified = $listener->processIpn();
+		} catch (Exception $e) {
+			error_log($e->getMessage());
+			exit(0);
+		}
+		if ($verified) {
+			$my_post = array(
+			  'post_title'    => $_REQUEST['first_name'] . ' ' . $_REQUEST['last_name'],
+			  'post_type'	  => 'ebook_order',
+			  'post_status'   => 'publish',
+			  'post_author'   => 1,
+			  'post_category' => array(8,39));
+			$custom = explode("|",$_REQUEST['custom']);
+			$mc_gross = number_format($_REQUEST['mc_gross'], 2, '.', ',');
+			$vat = get_option('vat_percent'); 
+			if ($vat > 0) {
+				$mc_gross = $_REQUEST['mc_gross'] - $_REQUEST['tax'];
+			}
+			$mc_gross = number_format($mc_gross, 2, '.', ',');
+			if (md5(NONCE_KEY . $custom[0] . $mc_gross) == $custom[1]) {
+				$post_id = wp_insert_post( $my_post, $wp_error );
+				foreach ($_REQUEST as $k => $v) {
+					update_post_meta($post_id, $k, $v);
+					@$order[$k] = $v;
+				}
+				$ebook_key = preg_replace("/[^a-zA-Z0-9]+/", "", $_REQUEST['ebook_key']);
+				$order['order_id'] = $post_id;
+				$order['password'] = $_REQUEST['payer_email'];
+				$ebook_order['ebook_key'][0] = $ebook_key;
+				$ebook_order['ebook'][0] = $custom[0];
+				$order['downloadlink'] = ebook_download_link($ebook_order);
+				update_post_meta($post_id,'ebook_key',$ebook_key);
+				update_post_meta($post_id,'downloads',0);
+				update_post_meta($post_id,'ebook',$custom[0]);
+				$attachment = ebook_attachment($custom[0]);
+				global $attachment;
+				mail(get_option( 'admin_email' ), 'eBook store for WordPress - Verified Order Received', $listener->getTextReport() . print_r($attachment,true));
+				//wp_mail($_REQUEST['payer_email'],get_option('email_delivery_subject'),'Email delivery text');
+				$ebook_email_delivery = array('to' => $_REQUEST['payer_email'], 'subject' => get_option('email_delivery_subject'), 'text' => get_option('email_delivery_text'),'attachment' => $attachment, 'order' => $order);
+				if (pathinfo($attachment[0]['file'],PATHINFO_EXTENSION) == 'pdf' && get_option('encrypt_pdf')) {
+					add_action( 'plugins_loaded', 'ebook_encrypt_pdf', 99 );
+				}
+				
+				add_action( 'plugins_loaded', 'ebook_email_delivery', 100 );
+				//array('to' => $_REQUEST['payer_email'], 'subject' => get_option('email_delivery_subject'), 'text' => 'teeext', 'file' => $attachment[0]['file'])
+			} else {
+				mail(get_option( 'admin_email' ), 'eBook store for WordPress - Possible fraud attempt ', $listener->getTextReport());
+			}
 	
+		} else {
+			mail(get_option( 'admin_email' ), 'eBook store for WordPress - Possible fraud attempt', $listener->getTextReport());
+		}
+		
+	}
 }
 add_action( 'init', 'ebook_create_post_type' );
 add_action( 'init', 'ebook_process_download', 100 );
