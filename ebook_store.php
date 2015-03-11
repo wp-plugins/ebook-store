0c5119f8d898ff7fa6cc0cac7cc4a479e4044c47
motov.net <motov.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 18:14:02 +0000

    new version with permalink bug fix
    git-svn-id: https://plugins.svn.wordpress.org/ebook-store/tags/3.5@1110540 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ebook_store.php
+++ ebook_store.php
@@ -5,7 +5,7 @@ Plugin URI: http://shopfiles.com/
 Description: A powerful tool for selling ebooks with wordpress
 Author: Deian Motov
 Author URI:http://shopfiles.com/
-Version: 3.4
+Version: 3.5
 License: GPLv2
 */
 
