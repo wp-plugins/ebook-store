7e31b18789f866d79a24298aefddee0186cc3703
motov.net <motov.net@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-03 11:03:25 +0000

    huge update in order handling and upgrades from free to pro version, new PDF DRM features added
    git-svn-id: https://plugins.svn.wordpress.org/ebook-store/tags/4.0@1152161 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/ebook_store_settings.js
@@ -0,0 +1,28 @@
+jQuery(function() {
+	if (ebook_store_license_key != '') {
+		jQuery.getJSON('https://www.shopfiles.com/api/wordpress_license.php?product=ebook_store&key=' + ebook_store_license_key,function(data) {
+			if (data.found == 1) {
+				console.log(data);
+				jQuery('.goPro').last().remove();
+				jQuery('.goPro').last().remove();
+				//accept=".pdf,.zip"
+				jQuery('#ebook_wp_custom_attachment_ebook').attr('accept','.pdf,.zip,.mobi,.epub,.txt');
+			} else {
+				alert(data.error);
+				ebook_store_no_license();
+			}
+		});
+	} else {
+		ebook_store_no_license();
+	}
+});
+function ebook_store_no_license() {
+	jQuery('.goPro input,.goPro2').click(function(e) {
+	jQuery('.goPro input').prop('checked',false);
+		if (confirm('Sorry, this feature is available in the Pro version only, would you like to upgrade now?')) {
+			window.location = 'http://www.shopfiles.com/index.php/products/wordpress-ebook-store';
+		}
+	});
+	jQuery('.goPro, .goPro2').css({background: '#FFB0B0', opacity: 1});
+	jQuery('input[type=radio].goPro2').remove();
+}
